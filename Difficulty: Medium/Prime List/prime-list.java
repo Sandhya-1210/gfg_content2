d990afbdfe6276dd993155fe85cae4ce67b22e5c
Sandhya-1210 <167778200+Sandhya-1210@users.noreply.github.com>
2025-05-03 07:48:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prime List/prime-list.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Node {
+    Node next;
+    int val;
+
+    public Node(int data) {
+        val = data;
+        next = null;
+    }
+}
+
+class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t;
+        t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+
+            Node head, tail;
+            String s[] = in.readLine().trim().split(" ");
+            int num = Integer.parseInt(s[0]);
+            head = new Node(num);
+            tail = head;
+            for (int i = 1; i < s.length; i++) {
+                num = Integer.parseInt(s[i]);
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            Node temp = ob.primeList(head);
+            while (temp != null) {
+                out.print(temp.val + " ");
+                temp = temp.next;
+            }
+            out.println();
+            out.println("~");
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+/*
+class Node{
+    Node next;
+    int val;
+    public Node(int data){
+        val=data;
+        next=null;
+    }
+}
+*/
+
+class Solution {
+    Node primeList(Node head) {
+        // code here
+        Node cur=head;
+        while(cur!=null){
+            cur.val=getNearestPrime(cur.val);
+            cur=cur.next;
+            
+        }
+        return head;
+        
+    }
+    private boolean isPrime(int n){
+        if(n<2) return false;
+        if(n==2) return true;
+        if(n%2==0) return false;
+        for(int i=3;i*i<=n;i+=2){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+    private int getNearestPrime(int n){
+        int lower=n,upper=n;
+        while(true){
+            if(isPrime(lower)) return lower;
+            if(isPrime(upper)) return upper;
+            lower--;
+            upper++;
+        }
+    }
+}
