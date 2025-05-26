ab492951338a3b328c63b3a92c7489eb274839d7
Sandhya-1210 <167778200+Sandhya-1210@users.noreply.github.com>
2025-05-26 21:28:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Insert in Sorted Circular Linked List/insert-in-sorted-circular-linked-list.java
@@ -0,0 +1,44 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+} */
+
+class Solution {
+    public Node sortedInsert(Node head, int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            newNode.next = newNode;
+            return newNode;
+        }
+
+        Node current = head;
+        while (true) {
+            if (current.data <= data && data <= current.next.data) {
+                break;
+            }
+            if (current.data > current.next.data) {
+                if (data >= current.data || data <= current.next.data) {
+                    break;
+                }
+            }
+
+            current = current.next;
+            if (current == head) {
+                break;
+            }
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+        if (head.data > data) {
+            return newNode;
+        }
+
+        return head;
+    }
+}
