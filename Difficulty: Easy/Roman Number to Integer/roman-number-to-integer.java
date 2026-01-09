23ec5c90a7e6942836638d8bc3f9ad72405c523d
Sandhya-1210 <167778200+Sandhya-1210@users.noreply.github.com>
2026-01-09 12:23:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.java
+++ Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.java
@@ -1,37 +1,28 @@
 class Solution {
+    public int romanTod(char c){
+        if(c=='I') return 1;
+        if(c=='V') return 5;
+        if(c=='X') return 10;
+        if(c=='L') return 50;
+        if(c=='C') return 100;
+        if(c=='D') return 500;
+        else return 1000;
+    }
     public int romanToDecimal(String s) {
         // code here
-        int no =0;
-        int n=s.length();
-        Map<Character, Integer>m=new HashMap<>();
-        m.put('I',1);
-        m.put('V',5);
-        m.put('X',10);
-        m.put('L',50);
-        m.put('C',100);
-        m.put('D',500);
-        m.put('M',1000);
-        for(int i=0;i<n;i++){
-            if(i<n-1){
-                char ch = s.charAt(i);
-                char ch2 = s.charAt(i+1);
-                if(ch==ch2){
-                    no+=m.get(ch)+m.get(ch);
-                    i++;
-                }
-                else if(m.get(ch)>m.get(ch2)){
-                    no+=m.get(ch);
-                }
-                else{
-                    int val = m.get(ch2) - m.get(ch);
-                    no+=val;
-                    i++;
-                }
-            }else{
-                no+=m.get(s.charAt(i));
+        int i=0;
+        int ans=0;
+        while(i<s.length()){
+            if(i!=s.length()-1 && romanTod(s.charAt(i))<romanTod(s.charAt(i+1))){
+                ans+=romanTod(s.charAt(i+1))-romanTod(s.charAt(i));
+                i=i+2;
+            }
+            else{
+                ans+=romanTod(s.charAt(i));
+                i++;
             }
         }
+        return ans;
         
-        return no;
     }
 }
