c28f5cf344e99d5d972a031611eb55472ba8d805
Sandhya-1210 <167778200+Sandhya-1210@users.noreply.github.com>
2026-01-10 15:09:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void dfs(int []node,boolean [][]visited,char[][]grid)
+    {
+        int i=node[0];
+        int j=node[1];
+        visited[i][j]=true;
+        int child[][]={{i+1, j}, {i-1, j}, {i, j+1}, {i, j-1},{i+1, j+1}, {i+1, j-1}, {i-1, j-1}, {i-1, j+1}};
+        for(int k=0;k<8;k++)
+        {
+            int ci=child[k][0];
+            int cj=child[k][1];
+            if(ci>=0 && ci<grid.length && cj>=0 && cj<grid[0].length && grid[ci][cj] == 'L'){
+                if(!visited[ci][cj]){
+                    dfs(new int[]{ci, cj}, visited, grid);
+                }
+            }
+        }
+    }
+
+    public int countIslands(char[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        boolean visited[][]=new boolean[n][m];
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='L' && !visited[i][j])
+                {
+                   c++;
+                   dfs(new int[]{i,j},visited,grid);
+                }
+            }
+            
+        }
+        return c;
+        
+    }
+}
